40d1d3cb85362ee14aa9eaf2c2dc61f078417571
ivanrobuschi <robuschiivan@gmail.com>
2016-06-05 18:33:17 +0200

    Each class has its own test class

---
+++ PANserver/MaskGeneratorTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using FluentAssertions;
+
+namespace PANserver
+{
+    class MaskGeneratorTest
+    {
+        [TestCase("1234567890123456")]
+        public void CreateMaskShouldCreateMaskedPanFromPan(string testPAN)
+        {
+            var sut = new MaskGenerator();
+            string maskedPAN = sut.CreateMask(testPAN);
+            testPAN.Substring(0, 6).Should().Be(maskedPAN.Substring(0, 6));
+            testPAN.Substring(12, 4).Should().Be(maskedPAN.Substring(12, 4));
+            for (int i = 6; i <= 11; i++)
+            {
+                int parsedChar;
+                char currentChar = maskedPAN[i];
+                string currentCharString = currentChar.ToString();
+                bool isANumber = int.TryParse(currentCharString, out parsedChar);
+                isANumber.Should().BeFalse();
+            }
+        }
+
+    }
+}
--- PANserver/PANserverTest.cs
+++ PANserver/PANserverTest.cs
@@ -9,7 +9,8 @@ namespace PANserver
     {
         //List<string> testMaskList = new List<string>();
         //List<string> testPanList = new List<string>();
-        string testFileName = @"C:\Temp\PANsTest.txt";
+        //string testFileName = @"C:\Temp\PANsTest.txt";
+        string testFileName;
         List<string> testExpectedPanList = new List<string>();
         List<string> testExpectedMaskList = new List<string>();
 
@@ -84,47 +85,6 @@ namespace PANserver
             var sut = new PANserver();
             string actualMask = sut.GetMask(PAN, testFileName);
             actualMask.Should().Be(expectedMask);
-        }
-
-        [TestCase("1234567890123456")]
-        public void CreateMaskShouldCreateMaskedPanFromPan(string testPAN)
-        {
-            var sut = new MaskGenerator();
-            string maskedPAN = sut.CreateMask(testPAN);
-            testPAN.Substring(0, 6).Should().Be(maskedPAN.Substring(0, 6));
-            testPAN.Substring(12, 4).Should().Be(maskedPAN.Substring(12, 4));
-            for (int i = 6; i <= 11; i++)
-            {
-                int parsedChar;
-                char currentChar = maskedPAN[i];
-                string currentCharString = currentChar.ToString();
-                bool isANumber = int.TryParse(currentCharString, out parsedChar);
-                isANumber.Should().BeFalse();
-            }
-        }
-
-        [Test]
-        public void SaveListsShouldWritePANsAndMasksListsToFile()
-        {
-            //This test needs that sut.CreateLists is working correctly
-            var server = new PANserver();
-            var sut = new PANArchiveManager();
-            server.panList.Add("1234567890123456");
-            server.panList.Add("2345678901234567");
-            server.maskList.Add("123456UHGTRA3456");
-            server.maskList.Add("234567BDJXHW4567");
-            testExpectedPanList.Clear();
-            testExpectedMaskList.Clear();
-            testExpectedPanList.Add("1234567890123456");
-            testExpectedPanList.Add("2345678901234567");
-            testExpectedMaskList.Add("123456UHGTRA3456");
-            testExpectedMaskList.Add("234567BDJXHW4567");
-            sut.SaveLists(testFileName, server.panList, server.maskList);
-            sut.CreateLists(testFileName, out server.panList, out server.maskList);
-            server.panList.Should().BeEquivalentTo(testExpectedPanList);
-            server.maskList.Should().BeEquivalentTo(testExpectedMaskList);
-
-            //metodi: panExists, createMask, addData
         }
     }
 }
