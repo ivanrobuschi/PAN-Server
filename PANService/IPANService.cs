7b25d52dbfafb237fe889f6eea8288a6d74ad447
ivanrobuschi <robuschiivan@gmail.com>
2016-06-06 22:05:07 +0200

    Added Web Service

---
+++ PANService/IPANService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace PANService
+{
+    // NOTA: è possibile utilizzare il comando "Rinomina" del menu "Refactoring" per modificare il nome di interfaccia "IService1" nel codice e nel file di configurazione contemporaneamente.
+    [ServiceContract]
+    public interface IPANService
+    {
+
+        [OperationContract]
+        string GetPAN(string mask);
+
+        [OperationContract]
+        string GetMask(string PAN);
+
+    }
+
+
+}
---
+++ PANService/PANService.svc.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace PANService
+{
+    // NOTA: è possibile utilizzare il comando "Rinomina" del menu "Refactoring" per modificare il nome di classe "Service1" nel codice, nel file svc e nel file di configurazione contemporaneamente.
+    // NOTA: per avviare il client di prova WCF per testare il servizio, selezionare Service1.svc o Service1.svc.cs in Esplora soluzioni e avviare il debug.
+    public class PANService : IPANService
+    {
+        public string GetMask(string PAN)
+        {
+            var server = new PANserver.PANserver(null);
+            string mask = server.GetMask(PAN);
+            return mask;
+        }
+
+        public string GetPAN(string mask)
+        {
+            var server = new PANserver.PANserver(null);
+            string PAN = server.GetPAN(mask);
+            return PAN;
+        }
+    }
+}
